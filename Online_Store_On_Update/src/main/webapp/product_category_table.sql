8188e99b1791dc8f5ef512039b6fcc3ae6a79332
Mohanaa Vishnu Priya Durla <71810443+dmvpriya@users.noreply.github.com>
2024-05-14 14:56:38 +0530

    Delete Online_Store_On_Update directory

--- Online_Store_On_Update/src/main/webapp/product_category_table.sql
+++
@@ -1,7 +0,0 @@
-create table products_category(prod_category_id int primary key,prod_category_name varchar(20));
-
-insert into products_category values(1,'fruits');
-insert into products_category values(2,'dairy');
-insert into products_category values(3,'non veg');
-insert into products_category values(4,'vegetables');
-
