e96d0ce52cd0312d5eb1cae25f381c4326548779
Mohanaa Vishnu Priya Durla <71810443+dmvpriya@users.noreply.github.com>
2024-05-28 15:25:35 +0530

    Delete ORM_Many_To_One_Example directory

--- ORM_Many_To_One_Example/src/main/java/com/spring/orm/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.orm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.orm.model.dmvp_Department_ORM_;
-
-public interface DepartmentRepository extends JpaRepository<dmvp_Department_ORM_, Integer> {
-
-}
--- ORM_Many_To_One_Example/src/main/java/com/spring/orm/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.orm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.orm.model.dmvp_Employee_ORM_;
-
-public interface EmployeeRepository extends JpaRepository<dmvp_Employee_ORM_, Integer> {
-
-}
